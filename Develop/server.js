de5fadb15ef63c0e2af35c9529cc56d3f8273611
verlitas <55808608+verlitas@users.noreply.github.com>
2019-11-14 21:28:14 -0700

    coding routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,4 @@
-//Dependencies we will need
+// Dependencies we will need
 // ====================================
 const express = require("express");
 const fs = require("fs");
@@ -6,20 +6,32 @@ const util = require("util");
 const path = require("path")
 // ====================================
 
-// Express Config
-// =====================================
+// Express Config 
+// ====================================
 //Tells node that we are creating an "express" server
 const app = express();
-//Set the server PORT
+// Set the server PORT
 const PORT = process.env.PORT || 8000;
-// Sets up the Express app to handle data parsing
+// Sets up the Express app to handle data parsing (Middleware)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// ======================================
-
 app.use(express.static("public"))
+// ====================================
 
-app.get("/api/notes", function (req, res) {
+// Routes
+// ====================================
+// /
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public.index.html"));
+});
+
+// notes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//api/notes
+app.get("/api/notes", (req, res) => {
     res.json([{
         id: 1,
         text: "Hello",
@@ -27,10 +39,14 @@ app.get("/api/notes", function (req, res) {
     }])
 })
 
-app.get("*", function (req, res) {
+//all
+app.get("*", (req, res) => {
     res.redirect("/");
 });
 
+// Listener
+// ====================================
 app.listen(PORT, () =>
     console.log(`App listening on PORT: ${PORT}`)
 )
+// ====================================
